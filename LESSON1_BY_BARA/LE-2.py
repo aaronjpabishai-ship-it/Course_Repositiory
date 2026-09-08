917c6c2b42185f3a4e5042dcf005b7c202c56e60
aaronjpabishai-ship-it <aaronjpabishai@gmail.com>
2026-09-08 15:20:41 +0530

    Lesson 3 by BARA YT

---
+++ LESSON1_BY_BARA/LE-2.py
@@ -0,0 +1,32 @@
+#VARIABLES: A LABEL THAT CONTAINS SOMETHING
+
+name = "Aaron"  # The variable to use instead of changing the names one by one
+
+print("My name is", name)
+print(name, "is learning Python")
+print(name, "wants to become a python expert")
+
+name = "Aaron"  # The variable to use instead of changing the names one by one
+language = "Python"
+
+print("My name is", name)
+print(name, "is learning", language)
+print(name, "wants to become a", language, "expert")
+
+#variables can be used to store different types of data, such as strings, numbers, and more. They allow you to easily update and manage information in your code without having to change it in multiple places.
+
+
+#Python Challenge:
+#-Print the following three lines
+#-Add a variable to make it dynamic
+
+print("info@datawithbaraa.com")
+print("support@datawithbaraa.com")
+print("www.datawithbaraa.com")
+
+#with variable
+
+name = "datawithbaraa"
+print("info@",name, ".com")
+print("support@",name,".com")
+print("www.",name,".com")
---
+++ LESSON1_BY_BARA/LE-3.py
@@ -0,0 +1,10 @@
+#FUNCTION
+#1. INPUT() get something from the user
+
+name = input("Enter your name")
+print("You are", name)
+
+
+name = input("Enter your name")
+country = "Germany"
+print(name, "is from", country)
