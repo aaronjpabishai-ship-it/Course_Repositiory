b3e9b2d96a6698ba3547914f0ae837f5b9a58819
aaronjpabishai-ship-it <aaronjpabishai@gmail.com>
2026-09-10 22:53:40 +0530

    Lesson 4 -- Lesson By Baara YT

---
+++ LESSON1_BY_BARA/LE-4.py
@@ -0,0 +1,81 @@
+#DATA TYPES
+# 
+# a = 10 #INT 
+# b = 10.7 #float
+# c = "Hello" #str
+# d = "Hi" #str
+# e = "1234" #str"
+# f = True #boolean and Case Sensitive
+# g = False #boolean
+# h = None #Nonetype
+# i = "" #treated as a string but blank
+# j = " " #str - Empty Space
+# 
+#// Functions + Data types
+#Functions : are a block of code -- syntax:function_name(value)  Ex:
+# print('hello there!')
+# type(77) 
+# 
+#// Methods : are functions that belong to an object -- syntax: value.method_name() EX:
+# "Hello there!".upper()
+# 50 .bit_length()
+
+
+#EXAMPLES:
+
+# text = "HELLO"
+# number = 10
+
+# print(text)
+# print(number)
+
+# text = "HELLO"
+# number = 10
+
+# print (type (text))
+# print(type(number))
+
+# text = "HELLO"
+# number = 10
+
+# print (len(text))
+# #print(len(number))
+
+
+
+# text = "HELLO"
+# number = 10
+
+# print(text.lower())
+# print(number.bit_length())
+
+
+
+#Python Challenge:
+#-Create 5 variables: Your Age, Your Height, YOur name, Are you a Student?, Something with no value yet.
+#Print the values, data types, lengths of all variables.
+
+age = 27
+height = 5.11
+name = "Aaron"
+is_student = True
+variable = None
+
+print(age)
+print(height)
+print(name)
+print(is_student)
+print(variable)
+
+
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))
+print(type(variable))
+
+#print(len(age))
+#print(len(height))
+print(len(name))
+#print(len(is_student))
+#print(len(variable))
